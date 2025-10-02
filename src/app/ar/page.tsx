df0d4963db715efd38c972fca2b99af15d455707
Chester Chu <chester@spe3d.co>
2025-10-02 14:05:36 +0800

    update: ar page ui

--- src/app/ar/page.tsx
+++ src/app/ar/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import imgBtnHome from "/public/assets/images/btn_home.png";
 import imgBackPlane from "/public/assets/images/back_plane.png";
 // import imgBtnPlayAr from "/public/assets/images/btn_play_ar.png";
+import imgBackPlaneWordingStep3 from "/public/assets/images/back_plane_wording_step3.png";
 
 export default function ARPage() {
   const mvRef = useRef<any>(null);
@@ -26,7 +27,7 @@ export default function ARPage() {
   }, []);
 
   return (
-    <div className="p-6 relative w-full h-[100dvh] flex flex-col items-center justify-center bg-black text-white">
+    <div className="p-6 relative w-full h-[100dvh] flex flex-col items-center justify-center bg-[#f2e18d] text-white">
       <button
         className="absolute top-8 right-8 z-[9999]"
         onClick={() => router.back()}
@@ -34,7 +35,7 @@ export default function ARPage() {
         <Image src={imgBtnHome} alt="imgBtnHome" width={56} height={56} />
       </button>
 
-      <div className="w-[300px] h-[300px] p-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 flex flex-col items-center justify-center bg-no-repeat bg-contain bg-center">
+      <div className="w-[375px] h-[375px] p-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 flex flex-col items-center justify-center bg-no-repeat bg-contain bg-center">
         <Image
           src={imgBackPlane}
           alt="imgBackPlane"
@@ -43,7 +44,19 @@ export default function ARPage() {
           className="opacity-50 object-contain pointer-events-none"
         />
 
-        <h2 className="my-6 text-2xl font-bold">AR 模式頁面</h2>
+        {/* <h2 className="my-6 text-2xl font-bold">AR 模式頁面</h2>
+        <p className="text-center text-sm opacity-60">
+          若 AR 已結束，你可以點擊首頁返回。
+        </p> */}
+
+        <div className="relative z-10 flex flex-col items-center">
+          <Image
+            src={imgBackPlaneWordingStep3}
+            alt="imgBackPlaneWordingStep3"
+            width={240}
+            height={50}
+          />
+        </div>
 
         <model-viewer
           id="model-viewer"
@@ -76,10 +89,6 @@ export default function ARPage() {
           />
         </button> */}
         </model-viewer>
-
-        <p className="text-center text-sm opacity-60">
-          若 AR 已結束，你可以點擊首頁返回。
-        </p>
       </div>
     </div>
   );
